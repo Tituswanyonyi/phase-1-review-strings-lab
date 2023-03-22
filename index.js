1c28d7efe87e22d0c156c09cb23fd399a282f9cd
titus <titus.unavailable@student.moringaschool.com>
2023-03-22 13:19:15 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, ';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
